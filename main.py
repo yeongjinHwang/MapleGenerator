0e95cede9e4263c3d0bc8ab021957476eff688bd
yeongjinHwang <yeongjin.gongjin@gmail.com>
2024-12-25 18:41:53 +0900

    add hair,clothesSeg

--- main.py
+++ main.py
@@ -1,23 +1,52 @@
-from utils.image import load_image, visualize_parts, visualize_combined_parts
-from utils.model import load_model, runs_image
+import json
+from utils.combine import combine_segments, visualize_segments, process_hair_predictions
+from utils.model import hair_loader, hair_infer, clothes_loader, clothes_infer
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 
-from ultralytics import YOLO
-import yaml
 
-image_path = "./data/" + "bts_jhin.jpg"
-image = load_image(image_path) 
+# 클래스 매핑 정의
+CLASS_MAPPING = {
+    "bag": "top",
+    "coat": "top",
+    "jacket": "top",
+    "shirt": "top",
+    "t-shirt": "top",
+    "pants": "bottom",
+    "shorts": "bottom",
+    "skirt": "bottom",
+    "shoes": "shoes",
+    "dress": "suit",
+    "hair" : "hair"
+}
 
-# 모델 로드
-hair_model = YOLO("runs/segment/train/weights/best.pt")
-clothes_model = YOLO("runs/detect/train/weights/best.pt")
+def main():
+    # 이미지 경로
+    image_path = "./data/bts_jhin.jpg"
 
-# Hair Segmentation 결과 추론
-hair_results = runs_image(hair_model, image)
-print("Hair Segmentation 결과:", hair_results)
+    # 헤어 세그멘테이션
+    hair_model = hair_loader()
+    hair_results = hair_infer(hair_model, image_path)
+    hair_predictions = process_hair_predictions(hair_results)
 
-# Clothes Detection 결과 추론
-clothes_results = runs_image(clothes_model, image)
-print("Clothes Detection 결과:", clothes_results)
+    # 의류 세그멘테이션
+    clothes_client = clothes_loader()
+    clothes_result = clothes_infer(clothes_client, image_path)
+    if isinstance(clothes_result, str):
+        clothes_result = json.loads(clothes_result)
+    predictions = clothes_result.get("predictions", [])
 
-# 두 결과를 합쳐 시각화
-visualize_combined_parts(image, hair_results, clothes_results)
+    # 헤어 결과와 의류 결과 병합
+    all_predictions = predictions + hair_predictions
+
+    # 재분류 및 세그먼트 결합
+    reclassified_cropped_images = combine_segments(image_path, all_predictions, CLASS_MAPPING)
+
+    # 시각화
+    visualize_segments(reclassified_cropped_images)
+
+    print(reclassified_cropped_images)
+
+if __name__ == "__main__":
+    main()
